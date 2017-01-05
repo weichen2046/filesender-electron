aa89496858af91dd02bad5886ce105538ed92cb9
Chen Wei <weichen2046@gmail.com>
2017-01-05 23:18:37 +0800

    Use target property of webpack config with angular-cli.
    Target property don't support by angular-cli by now, so need a fork
    repo of angular-cli. Please refer to:
    https://github.com/weichen2046/angular-cli
    
    How to use a fork angular-cli, please refer to the section "Development
    Hints for hacking on angular-cli" of README.md file of project
    angular-cli.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { remote } from 'electron';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+  }
+
+  public newWindow() {
+    let newW = new remote.BrowserWindow({
+      width: 800,
+      height: 600
+    });
+
+    newW.on('close', () => newW = null);
+    newW.loadURL('https://www.google.com.hk');
+    newW.show();
+  }
 }
