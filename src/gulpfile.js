22bb8c20c65a99cd7880197244c254ee6b5a96c0
Chen Wei <weichen2046@gmail.com>
2017-02-25 10:54:38 +0800

    Enable debugging main process in VS Code.

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.gulp.json');
 
 gulp.task('default', () => {
-  return tsProject.src()
-      .pipe(tsProject())
-      .js.pipe(gulp.dest('../dist'));
+  var tsResult = tsProject.src()
+      .pipe(sourcemaps.init())
+      .pipe(tsProject());
+
+  return tsResult.js
+      .pipe(sourcemaps.write('../dist'))
+      .pipe(gulp.dest('../dist'));
 });
