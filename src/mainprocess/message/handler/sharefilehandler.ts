98cf4727b9789022ac4870deab85f0fa5aa264dc
Chen Wei <weichen2046@gmail.com>
2017-05-09 20:25:07 +0800

    feat: request send file before real send file

--- src/mainprocess/message/handler/sharefilehandler.ts
+++ src/mainprocess/message/handler/sharefilehandler.ts
@@ -32,12 +32,67 @@ export class ShareFileHandler extends AsyncMsgHandler {
       properties: ['openFile', 'multiSelections']
     }, (paths) => {
       //console.log('selected paths:', paths, 'phone:', this._phone);
-      // TODO: connect phone and share file
       // send file sending request
-      // send file
-      //this.sendSendingFileRequestTcp(paths);
       this._paths = paths;
-      this.sendFiles(paths);
+      this.sendSendingFileRequestTcp(paths);
+    });
+  }
+
+  private sendSendingFileRequestTcp(paths: string[]) {
+    if (!this._phone.isAuthenticated()) {
+      console.log('can not share file to unauthenticated phone');
+      return;
+    }
+    let client = new net.Socket();
+    client.connect(this._phone.tcpPort, this._phone.address, () => {
+      console.log('tcp client connected');
+      // 4 bytes data version
+      // 4 bytes cmd
+      // 4 bytes access token length
+      // x bytes access token
+      // 4 bytes file list length
+      // 4 bytes nth file name length
+      // x bytes nth file name
+
+      let phone = this._phone;
+      let intBuff = Buffer.alloc(4);
+      // write data version
+      intBuff.writeInt32BE(config.tcp.dataVer, 0);
+      client.write(intBuff);
+      this._allDataSent += 4;
+      // write cmd
+      intBuff.writeInt32BE(config.cmd.pc.cmd_sending_file_request, 0);
+      client.write(intBuff);
+      this._allDataSent += 4;
+      // write access token length
+      intBuff.writeInt32BE(phone.accessToken.length, 0);
+      client.write(intBuff);
+      this._allDataSent += 4;
+      // write access token
+      client.write(phone.accessToken);
+      this._allDataSent += phone.accessToken.length;
+      // write file list length
+      intBuff.writeInt32BE(paths.length, 0);
+      client.write(intBuff);
+      this._allDataSent += 4;
+
+      paths.forEach(fullpath => {
+        // get file name from path
+        let filename = path.basename(fullpath);
+        // write Nth file name length
+        let fileNameLength = Buffer.byteLength(filename);
+        intBuff.writeInt32BE(fileNameLength, 0);
+        client.write(intBuff);
+        this._allDataSent += 4;
+        // write Nth file name
+        client.write(filename);
+        this._allDataSent += fileNameLength;
+      });
+      client.end();
+    });
+    client.on('close', () => {
+      console.log('send file request tcp client closed, all data send:', this._allDataSent);
+      client = null;
     });
   }
 
@@ -55,7 +110,7 @@ export class ShareFileHandler extends AsyncMsgHandler {
       // 4 bytes cmd
       // 4 bytes access token length
       // x bytes access token
-      // 4 bytes paths length
+      // 4 bytes file list length
       // 4 bytes Nth file name length
       // x bytes Nth file name
       // 4 bytes Nth file content length
@@ -78,7 +133,7 @@ export class ShareFileHandler extends AsyncMsgHandler {
       // write access token
       client.write(phone.accessToken);
       this._allDataSent += phone.accessToken.length;
-      // write paths length
+      // write file list length
       intBuff.writeInt32BE(paths.length, 0);
       client.write(intBuff);
       this._allDataSent += 4;
@@ -129,66 +184,6 @@ export class ShareFileHandler extends AsyncMsgHandler {
       } else {
         setTimeout(this.asyncSendFileOrdered.bind(this), 0);
       }
-    });
-  }
-
-  private sendSendingFileRequestTcp(paths: string[]) {
-    if (!this._phone.isAuthenticated()) {
-      console.log('can not share file to unauthenticated phone');
-      return;
-    }
-    let client = new net.Socket();
-    client.connect(this._phone.tcpPort, this._phone.address, () => {
-      console.log('tcp client connected');
-      // 4 bytes data version
-      // 4 bytes cmd
-      // 4 bytes access token length
-      // x bytes access token
-      // 4 bytes paths length
-      // 4 bytes nth path length
-      // x bytes nth path
-      let phone = this._phone;
-      let dataLen = 4 + 4 + 4 + phone.accessToken.length + 4;
-      let allPathByteLen = 0;
-      paths.forEach(fullpath => {
-        allPathByteLen += 4;
-        allPathByteLen += fullpath.length;
-      });
-      dataLen += allPathByteLen;
-      let msg = Buffer.alloc(dataLen);
-      // write version
-      let buffOffset = 0;
-      msg.writeInt32BE(config.tcp.dataVer, buffOffset);
-      // write cmd
-      buffOffset += 4;
-      msg.writeInt32BE(config.cmd.pc.cmd_sending_file_request, buffOffset);
-      // write access token length
-      buffOffset += 4;
-      msg.writeInt32BE(phone.accessToken.length, buffOffset);
-      // write access token
-      buffOffset += 4;
-      msg.write(phone.accessToken, buffOffset);
-      // write paths length
-      buffOffset += phone.accessToken.length;
-      msg.writeInt32BE(paths.length, buffOffset);
-
-      buffOffset += 4;
-      paths.forEach(fullpath => {
-        // write nth path length
-        msg.writeInt32BE(fullpath.length, buffOffset);
-        // write nth path
-        buffOffset += 4;
-        msg.write(fullpath, buffOffset);
-        buffOffset += fullpath.length;
-      });
-      client.write(msg, () => {
-        console.log(`client write ${dataLen} bytes`);
-      });
-      client.end();
-    });
-    client.on('close', () => {
-      console.log('tcp client closed');
-      client = null;
     });
   }
 
